90b17077bb719e2e73c3185741ef88a50cb88385
Braedon Niemi <70549370+btniemi@users.noreply.github.com>
2021-02-03 08:47:09 -0700

    Added Test to anagram

--- Assignment1/anagram.py
+++ Assignment1/anagram.py
@@ -1,20 +1,7 @@
-string1 = "listen"
-string2 = "silent"
-
-string3 = "blaga"
-string4 = "blaau"
-
-string5 = "goodness gracious"
-string6 = "wow"
-
-string7 = "bad credit"
-string8 = "debit card"
-
-string9 = "Bad credit"
-string10 = "Debit card"
-
-
 # used https://www.geeksforgeeks.org/python-sorted-check-two-strings-anagram-not/ as a guide for this program
+"""dont really need the print statements in here unless you feel like they help you understand.
+ Could probably just comment that out or add a comments to help understand what going on"""
+
 
 def anagram(s1, s2):
     s1 = s1.lower()
@@ -22,14 +9,23 @@ def anagram(s1, s2):
     if len(s1) == len(s2):
         if (sorted(s1) == sorted(s2)):
             print("YES, is anagram.")
+            return True
         else:
             print("No, NOT anagrams.")
+            return False
     elif len(s1) != len(s2):
         print("No, NOT anagrams.")
+        return False
 
 
-anagram(string1, string2)
-anagram(string5, string6)
-anagram(string3, string4)
-anagram(string7, string8)
-anagram(string9, string10)
+def test_anagram():
+    assert anagram("listen","silent") == True
+def test_anagram():
+    assert anagram("blaga","blaau") == False
+def test_anagram():
+    assert anagram("goodness gracious", "blue") == False
+def test_anagram():
+    assert anagram("bad credit", "debit card") == True
+def test_anagram():
+    assert anagram("Bad CreDit", "Debit CaRd") == True
+
--- InClassExamples/TestingExamples/mySum.py
+++ InClassExamples/TestingExamples/mySum.py
@@ -4,4 +4,4 @@ def my_sum(x, y):
     return x + y
 
 def test_my_sum():
-    assert my_sum(2,2)==4
+    assert my_sum(2,2) == 4
