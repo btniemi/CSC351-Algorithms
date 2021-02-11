2d943ef258262cf0aff5f51b89195eb0c8f54c01
Braedon Niemi <70549370+btniemi@users.noreply.github.com>
2021-02-10 20:42:39 -0700

    Finished tma
    tried to work on knapsack still not getting there but tma is working just need to finsh up the check which should be easy and then make it work with file reading stuff if I can and write some tests

--- Assignment3/tma.py
+++ Assignment3/tma.py
@@ -3,10 +3,13 @@ import pytest
 """I used code from https://www.geeksforgeeks.org/stable-marriage-problem/ to help jumpstart my idea.
 I modified things to match the parameters of the project."""
 
+"""think nested 4 loops, use lists of lists, termination condition = all men have a mate
+what to do last is to figure out the file read and file write
+"""
+
 m_preference = [[3,2,5,1,4],[1,2,5,3,4],[4,3,2,1,5],[1,3,4,2,5],[1,2,4,5,3]]
 w_preference = [[3,5,2,1,4],[5,2,1,4,3],[4,3,5,1,2],[1,2,3,4,5],[2,3,4,1,5]]
 marriages = [(1,5),(2,2),(3,4),(4,3),(5,1)] #(boys,girls) girls saying no and men seeking
-
 
 def match(menPrefrence, womanPrefrence):
     if len(menPrefrence) != len(womanPrefrence):
@@ -33,35 +36,40 @@ def match(menPrefrence, womanPrefrence):
             #if this woman is free:
             if freeW[w-1] == False:
                 # man and woman get engaged
-                freeW[w-1] = m
+                freeW[w-1] = m + 1
                 freeM[m] = True
                 freeCount -= 1
             #else woman is engaged to man prime
             else:
                 mPrime = freeW[w-1]
-                newMan= m
+                prime = womanPrefrence[w-1].index(mPrime)
+                newMan = m + 1
+                man = womanPrefrence[w-1].index(newMan)
                 #if woman prefers man to man prime
-                if newMan < mPrime:
+                if man < prime:
                     freeW[w-1] = newMan
                     # man and woman get engaged
                     freeM[m] = True
                     # man prime set to free
-                    freeM[mPrime] = False
+                    freeM[mPrime-1] = False
+                    # manPrime throws away his preference for that woman from his list as they will not match
+
                 else:
                     """man prime and woman remain engaged"""
             i += 1
-    return print(freeM, freeW, sep="\r\n")
+    return print(freeM, freeW, sep="\r\n")  # mess with this return statement to get a better print of pairings
+                                            #print to a file the answer need help here to see if I am on the right path...
 
-    #print to a file the answer need help here to see if I am on the right path...
+
+
+
+matched = [(1, 5), (2, 4), (3, 3), (4, 2), (5, 1)]
 
 def check(matched):
-    #use the match function to check if pairings are bad or not correct this should be a basic call to the match function i think
-
-    """think nested 4 loops,
-    can pop from a list,
-    use lists of lists,
-    termination condition = all men have a mate
-    what to do last is to figure out the file read and file write
-    """
+    #for each boy first tuple
+        #check if match equals any other matched boy tuples pair
+            #if yes match == false
+            #else match == true
+    pass
 
 match(m_preference, w_preference)
