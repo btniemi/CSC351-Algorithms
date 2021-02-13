d6c54a2b009c2cdc852247e918e69967413dbc6e
Braedon Niemi <70549370+btniemi@users.noreply.github.com>
2021-02-12 21:59:46 -0700

    changes to knapsack

--- Assignment2/knapsack.py
+++ Assignment2/knapsack.py
@@ -1,6 +1,6 @@
 import pytest
-import sys
 import ast
+import sys
 
 """ I used code from https://www.geeksforgeeks.org/fractional-knapsack-problem/ to help kickstart my ideas,
 ended up useing most of the same code and tried to make it different but could not find another way. Not
@@ -62,7 +62,7 @@ itemsList = list
 maxValue = fillKnapsackByValue(itemsList, knapsack)
 print("The max value of your bag is: ", maxValue)
 
-
+"""not sure why this is broken now it sams that type object is not iterable something with taking in an argument messed this up but why???"""
 def test_knapsack():
     assert fillKnapsackByValue([(60, 10), (40, 40), (100, 20), (120, 30)], 50) == 240, "Should PASS"
 def test_knapsack():
