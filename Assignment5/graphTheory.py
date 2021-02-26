57080870f4f2842178f8fc76dee3553db01fd088
Braedon Niemi <70549370+btniemi@users.noreply.github.com>
2021-02-26 09:24:12 -0700

    started work on new assignment

---
+++ Assignment5/graphTheory.py
@@ -0,0 +1,16 @@
+graph = [[0, 1, 1, 1],
+         [1, 0, 1, 0],
+         [1, 1, 0, 1],
+         [1, 0, 1, 0]]
+
+
+def colorGraph(graph):
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
