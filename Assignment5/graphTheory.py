d29afed06a1fa3d79ae6477a196526d7c23ffcec
Braedon Niemi <70549370+btniemi@users.noreply.github.com>
2021-02-26 15:51:41 -0700

    work on new assignment
    I do not understand where to even go from here

--- Assignment5/graphTheory.py
+++ Assignment5/graphTheory.py
@@ -1,15 +1,17 @@
-graph = [[0, 1, 1, 1],
-         [1, 0, 1, 0],
-         [1, 1, 0, 1],
-         [1, 0, 1, 0]]
-
-
 def colorGraph(graph):
-    pass
+    result = []
+
+    return result
+
 
 
 def main():
-    pass
+    graph = [[0, 1, 1, 1],
+             [1, 0, 1, 0],
+             [1, 1, 0, 1],
+             [1, 0, 1, 0]]
+
+    colorGraph(graph)
 
 
 if __name__ == "__main__":
