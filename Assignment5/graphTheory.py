a9f56dcc499844f3b2c2a7a7900f4c10c55efb56
Braedon Niemi <70549370+btniemi@users.noreply.github.com>
2021-03-01 15:46:32 -0700

    updates

--- Assignment5/graphTheory.py
+++ Assignment5/graphTheory.py
@@ -6,6 +6,8 @@ def colorGraph(graph):
             result.append(idx)
             break
 
+    """i need to add a check here but not sure"""
+
     return result
 
 
@@ -14,7 +16,8 @@ def main():
              [1, 0, 1, 0],
              [1, 1, 0, 1],
              [1, 0, 1, 0]]
-    """result should equal [0,1,2,0]"""
+    """result should equal [0,1,2,1]"""
+    # graph returns [0,1,2,1]
 
     graph2 = [[0, 1, 1, 0, 0],
               [1, 0, 1, 1, 0],
@@ -22,9 +25,17 @@ def main():
               [0, 1, 1, 0, 1],
               [0, 0, 0, 1, 0]]
     """result should equal [0,1,2,0,1]"""
+    # graph2 returns [0,1,2,0,0]
+
+    graph3 = [[0, 1, 1],
+              [1, 0, 1],
+              [1, 1, 0]]
+    """result should be [0,0,1] how is this possible"""
+    #graph3 return [0,1,2]
 
     print(colorGraph(graph))
     print(colorGraph(graph2))
+    print(colorGraph(graph3))
 
 
 if __name__ == "__main__":
