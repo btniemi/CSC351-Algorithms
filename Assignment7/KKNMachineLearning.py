cd0f8a4f2c9836f9bbde8cea3a843ebed483da10
Braedon Niemi <70549370+btniemi@users.noreply.github.com>
2021-04-14 09:08:22 -0600

    KNN solution
    read over this and learn from it more

--- Assignment7/KKNMachineLearning.py
+++ Assignment7/KKNMachineLearning.py
@@ -1,7 +1,15 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.metrics import mean_squared_error
+from math import sqrt
+import seaborn as sns
+from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import BaggingRegressor
 
+#import abalone dataset
 url = (
     "https://archive.ics.uci.edu/ml/machine-learning-databases/abalone/abalone.data"
 )
@@ -21,12 +29,14 @@ abalone.columns = [
 print(abalone.head())
 
 abalone = abalone.drop("Sex", axis=1)
+#show data set based on rings
 abalone["Rings"].hist(bins=15)
 plt.show()
-
+#correlation matrix
 correlation_matrix = abalone.corr()
-correlation_matrix["Rings"]
+print(correlation_matrix["Rings"])
 
+#start code for KNN from scratch
 X = abalone.drop("Rings", axis=1)
 X = X.values
 y = abalone["Rings"]
@@ -48,4 +58,85 @@ k = 3
 nearest_neighbor_ids = distances.argsort()[:k]
 print(nearest_neighbor_ids)
 
-#START POINT IS AT VOTING OR AVERAGING OF MULTIPLE NEIGHBORS
+#voting or averageing of multiple neighbors
+nearest_neighbor_rings = y[nearest_neighbor_ids]
+print(nearest_neighbor_rings)
+#average for regression
+prediction = nearest_neighbor_rings.mean()
+print(prediction)
+
+#splitting data sets to training and testing
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=12345
+)
+#fitting knn regression into scikit-learn to the abalone dataset
+knn_model = KNeighborsRegressor(n_neighbors=3)
+knn_model.fit(X_train, y_train)
+#inspecting fit for data sets
+train_preds = knn_model.predict(X_train)
+mse = mean_squared_error(y_train, train_preds)
+rmse = sqrt(mse)
+print(rmse)
+
+test_preds = knn_model.predict(X_test)
+mse = mean_squared_error(y_test, test_preds)
+rmse = sqrt(mse)
+print(rmse)
+#plot the fit of the model
+cmap = sns.cubehelix_palette(as_cmap=True)
+f, ax = plt.subplots()
+points = ax.scatter(
+    X_test[:, 0], X_test[:, 1], c=test_preds, s=50, cmap=cmap
+)
+f.colorbar(points)
+plt.show()
+
+cmap = sns.cubehelix_palette(as_cmap=True)
+f, ax = plt.subplots()
+points = ax.scatter(
+    X_test[:, 0], X_test[:, 1], c=y_test, s=50, cmap=cmap
+)
+f.colorbar(points)
+plt.show()
+
+#improving knn preformance using gridsearch
+parameters = {"n_neighbors": range(1, 50)}
+gridsearch = GridSearchCV(KNeighborsRegressor(), parameters)
+gridsearch.fit(X_train, y_train)
+print(gridsearch.best_params_)
+
+train_preds_grid = gridsearch.predict(X_train)
+train_mse = mean_squared_error(y_train, train_preds_grid)
+train_rmse = sqrt(train_mse)
+test_preds_grid = gridsearch.predict(X_test)
+test_mse = mean_squared_error(y_test, test_preds_grid)
+test_rmse = sqrt(test_mse)
+print(train_rmse)
+print(test_rmse)
+
+#weighted averages for gridsearch
+parameters = {
+    "n_neighbors": range(1, 50),
+    "weights": ["uniform", "distance"],
+}
+gridsearch = GridSearchCV(KNeighborsRegressor(), parameters)
+gridsearch.fit(X_train, y_train)
+print(gridsearch.best_params_)
+
+test_preds_grid = gridsearch.predict(X_test)
+test_mse = mean_squared_error(y_test, test_preds_grid)
+test_rmse = sqrt(test_mse)
+print(test_rmse)
+
+#using bagging to improve further
+best_k = gridsearch.best_params_["n_neighbors"]
+best_weights = gridsearch.best_params_["weights"]
+bagged_knn = KNeighborsRegressor(
+    n_neighbors=best_k, weights=best_weights
+)
+bagging_model = BaggingRegressor(bagged_knn, n_estimators=100)
+bagging_model.fit(X_train, y_train)
+test_preds_grid = bagging_model.predict(X_test)
+test_mse = mean_squared_error(y_test, test_preds_grid)
+test_rmse = sqrt(test_mse)
+print(test_rmse)
